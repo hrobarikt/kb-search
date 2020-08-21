2e3374bf46d03c76bd21fbb34d54c9f67eee22f4
Tomas Hrobarik <hrobarikt@snow.aspectworks.com>
2020-08-21 11:18:59 +0200

    input and styling

--- src/x-20581-kb-search/index.js
+++ src/x-20581-kb-search/index.js
@@ -1,13 +1,30 @@
 import { createCustomElement } from "@servicenow/ui-core";
 import snabbdom from "@servicenow/ui-renderer-snabbdom";
 import styles from "./styles.scss";
+import "@servicenow/now-card";
+import "@servicenow/now-icon";
 
 const view = (state, { updateState }) => {
-	return <div>Hello 👋</div>;
+	return (
+		<div>
+			<header>
+				<now-icon icon="magnifying-glass-outline"></now-icon>
+				<input
+					value={state.searchText}
+					on-input={(e) => updateState({ searchText: e.target.value })}
+				/>
+			</header>
+
+			<now-card>
+				search results for <b>{state.searchText}</b> here
+			</now-card>
+		</div>
+	);
 };
 
 createCustomElement("x-20581-kb-search", {
 	renderer: { type: snabbdom },
+	initialState: { searchText: "email" },
 	view,
 	styles,
 });
